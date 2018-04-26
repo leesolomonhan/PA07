be5412d883fcac6b4ea5927c3b91257c19398b36
leesolomonhan <35318114+leesolomonhan@users.noreply.github.com>
2018-04-25 21:41:09 -0400

    Update Slider.java

--- Slider.java
+++ Slider.java
@@ -5,12 +5,21 @@ import javax.swing.*;
 import javax.swing.event.*;
 
 public class Slider extends JFrame
-        implements ChangeListener {
+        implements ActionListener, ChangeListener {
 JSlider slide = new JSlider(0, 9, 0);
+JButton reset = new JButton("Reset");
 JTextField value = new JTextField();
 
+public static void main(String[]args) {
+  Slider x = new Slider();
+  x.setVisible(true);
+}
 public Slider() {
   setLayout(new BorderLayout());
+  setSize(500,200);
+  setTitle("Value");
+  setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
   JPanel center = new JPanel();
   JPanel end = new JPanel();
   end.setLayout(new FlowLayout());
@@ -27,11 +36,26 @@ public Slider() {
   value.setText("0");
   add("Center",center);
 
+  reset.addActionListener(this);
+  end.add(reset);
   add("South", end);
 }
 
+public void actionPerformed(ActionEvent e){
+  if (e.getSource() == reset){
+    reset();
+  }
+}
+private void reset(){
+  value.setText("0");
+  slide.setValue(0);
+}
   public void stateChanged(ChangeEvent e) {
     int values = slide.getValue();
     value.setText(""+values);
+  }
+  private void reset(){
+    value.setText("0");
+    slide.setValue(0);
   }
 }
